dd879b14117c284f6cbaf63633ae63a56ccb362c
Graeme Coupar <graeme@turbofish.tech>
2024-09-05 20:01:49 +0100

    feat: make it work (#2)
    This PR takes the basic implementation of this app (which I made while I
    was still allowed to push to main) and iterates on it in tandem with the
    work in https://github.com/grafbase/grafbase/pull/2090 (see that PR for
    context)
    
    I've also added some CI that'll build releases. Once this is merged I'll
    update the grafbase repo to use the binaries instead of building on
    every CI run.

---
+++ src/config.rs
@@ -0,0 +1,29 @@
+use std::fs::read_to_string;
+
+pub fn load() -> Config {
+    do_load().unwrap_or_default()
+}
+
+fn do_load() -> Option<Config> {
+    let path = std::env::var("WHAT_RUST_CHANGED_CONFIG").ok()?;
+
+    eprintln!("Loading config from {path}");
+
+    let data = read_to_string(path).expect("to be able to read named config file");
+
+    toml::from_str(&data).expect("invalid config format")
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+#[serde(rename_all = "kebab-case")]
+pub struct Config {
+    /// Packages that shouldn't automatically be included in test runs
+    pub ignore_test_packages: Vec<String>,
+
+    /// Packages that require docker for their tests and should be put
+    /// into a separate section of the output
+    pub docker_test_packages: Vec<String>,
+
+    #[serde(flatten)]
+    pub determinator_rules: determinator::rules::DeterminatorRules,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
 #![allow(unstable_name_collisions)]
 
-use determinator::{rules::DeterminatorRules, Determinator};
-use guppy::{
-    graph::{BuildTarget, BuildTargetKind, DependencyDirection},
-    CargoMetadata,
-};
+mod config;
+mod guppy_ext;
+
+use determinator::Determinator;
+use guppy::{graph::DependencyDirection, CargoMetadata};
+use guppy_ext::PackageMetadataExt;
+
+#[derive(serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct Report {
+    /// A string that can be passed to cargo build to limit to changed packages
+    cargo_build_specs: String,
+
+    /// A string that can be passed to cargo test to limit to changed packages
+    ///
+    /// This one should be used for platforms that do not support docker
+    cargo_test_specs: String,
+
+    /// A string that can be passed to cargo test to limit to changed packages
+    ///
+    /// This one should be used for platforms that support docker
+    cargo_docker_test_specs: String,
+
+    /// A string that can be passed to cargo build to limit only to changed binaries
+    cargo_bin_specs: String,
+
+    /// The full list of packages that have changed, useful for other CI filtering purposes
+    changed_packages: Vec<String>,
+
+    /// The full list of binaries that have changed, useful for other CI filtering purposes
+    changed_binaries: Vec<String>,
+}
 
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
 
+    let config = config::load();
+
+    eprintln!("Comparing metadata from {} to {}", &args[1], &args[2]);
+
     // guppy accepts `cargo metadata` JSON output. Use a pre-existing fixture for these examples.
     let old_metadata =
-        CargoMetadata::parse_json(std::fs::read_to_string(dbg!(&args[1])).unwrap()).unwrap();
+        CargoMetadata::parse_json(std::fs::read_to_string(&args[1]).unwrap()).unwrap();
     let old = old_metadata.build_graph().unwrap();
     let new_metadata =
         CargoMetadata::parse_json(std::fs::read_to_string(&args[2]).unwrap()).unwrap();
@@ -19,30 +50,67 @@ fn main() {
 
     let mut determinator = Determinator::new(&old, &new);
 
-    // The determinator supports custom rules read from a TOML file.
-    // let rules =
-    //     DeterminatorRules::parse(include_str!("../../../fixtures/guppy/path-rules.toml")).unwrap();
+    determinator.set_rules(&config.determinator_rules).unwrap();
 
-    determinator
-        .set_rules(DeterminatorRules::default_rules())
-        .unwrap();
+    eprintln!("Changed files:");
+    for path in &args[3..] {
+        eprintln!("- {path}");
+    }
+    eprintln!();
 
     // The determinator expects a list of changed files to be passed in.
-    determinator.add_changed_paths(dbg!(&args[3..]));
+    determinator.add_changed_paths(&args[3..]);
 
     let determinator_set = determinator.compute();
 
-    // determinator_set.affected_set contains the workspace packages directly or indirectly affected
-    // by the change.
-    let cargo_test_targets = determinator_set
+    let cargo_build_specs = determinator_set
         .affected_set
         .packages(DependencyDirection::Forward)
-        .filter(|package| package.has_test_targets())
         .flat_map(|package| ["-p", package.name()])
         .collect::<Vec<_>>()
         .join(" ");
 
-    let cargo_build_targets = determinator_set
+    let cargo_test_specs = determinator_set
+        .affected_set
+        .packages(DependencyDirection::Forward)
+        .filter(|package| package.has_test_targets())
+        .filter(|package| {
+            !config
+                .ignore_test_packages
+                .iter()
+                .any(|name| package.name() == name)
+        })
+        .filter(|package| {
+            !config
+                .docker_test_packages
+                .iter()
+                .any(|name| package.name() == name)
+        })
+        .flat_map(|package| ["-p", package.name()])
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    let cargo_docker_test_specs = determinator_set
+        .affected_set
+        .packages(DependencyDirection::Forward)
+        .filter(|package| package.has_test_targets())
+        .filter(|package| {
+            !config
+                .ignore_test_packages
+                .iter()
+                .any(|name| package.name() == name)
+        })
+        .flat_map(|package| ["-p", package.name()])
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    let changed_packages = determinator_set
+        .affected_set
+        .packages(DependencyDirection::Forward)
+        .map(|package| package.name().to_string())
+        .collect();
+
+    let cargo_bin_specs = determinator_set
         .affected_set
         .root_packages(DependencyDirection::Forward)
         .flat_map(|package| {
@@ -55,77 +123,26 @@ fn main() {
         .collect::<Vec<_>>()
         .join(" ");
 
+    let changed_binaries = determinator_set
+        .affected_set
+        .packages(DependencyDirection::Forward)
+        .flat_map(|package| {
+            package
+                .binary_targets()
+                .into_iter()
+                .map(|target| target.name().to_string())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
     let report = Report {
-        cargo_test_targets,
-        cargo_build_targets,
+        cargo_build_specs,
+        cargo_test_specs,
+        cargo_docker_test_specs,
+        cargo_bin_specs,
+        changed_packages,
+        changed_binaries,
     };
 
     println!("{}", serde_json::to_string(&report).unwrap())
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-pub struct Report {
-    cargo_test_targets: String,
-    cargo_build_targets: String,
-}
-
-trait PackageMetadataExt {
-    fn has_test_targets(&self) -> bool;
-    fn binary_targets(&self) -> Vec<BuildTarget<'_>>;
-}
-
-impl PackageMetadataExt for guppy::graph::PackageMetadata<'_> {
-    fn has_test_targets(&self) -> bool {
-        let package_root_path = self
-            .manifest_path()
-            .parent()
-            .expect("all packages to have manifests with one parent");
-
-        self.build_targets()
-            .filter(|target| {
-                matches!(
-                    target.kind(),
-                    BuildTargetKind::Binary | BuildTargetKind::LibraryOrExample(_)
-                )
-            })
-            .any(|target| {
-                let relative_path = target
-                    .path()
-                    .strip_prefix(package_root_path)
-                    .expect("targets to live inside package");
-
-                let Some(root_folder) = relative_path.components().next() else {
-                    return false;
-                };
-
-                let root_folder = root_folder.as_str();
-
-                // Unfortunately doesn't seem to be any way to tell whether something rooted in
-                // src actually has tests or not, so best to just assume they do
-                root_folder == "tests" || root_folder == "src"
-            })
-    }
-
-    fn binary_targets(&self) -> Vec<BuildTarget<'_>> {
-        let package_root_path = self
-            .manifest_path()
-            .parent()
-            .expect("all packages to have manifests with one parent");
-
-        self.build_targets()
-            .filter(|target| matches!(target.kind(), BuildTargetKind::Binary))
-            .filter(|target| {
-                let relative_path = target
-                    .path()
-                    .strip_prefix(package_root_path)
-                    .expect("targets to live inside package");
-
-                let Some(root_folder) = relative_path.components().next() else {
-                    return false;
-                };
-
-                root_folder.as_str() == "src" && relative_path.file_name() == Some("main.rs")
-            })
-            .collect()
-    }
 }
